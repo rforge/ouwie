22c541e38bfe73d8b1da96189af5b7e95e35121e
bcomeara <bcomeara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-22 23:37:00 +0000

    coolness, aka genetic algorithm seems to work
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ouwie@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/OUwie.dredge.R
+++ pkg/R/OUwie.dredge.R
@@ -1,4 +1,4 @@
-OUwie.dredge<-function(phy,data, criterion=c("aicc","aic","rjmcmc"), theta.max.k=3, sigma.max.k=3, alpha.max.k=3, root.station=TRUE, ip=1, lb=0.000001, ub=1000, max.generations=100, wait.generations=10, pop.size=NULL) {
+OUwie.dredge<-function(phy,data, criterion=c("aicc","aic","rjmcmc"), theta.max.k=3, sigma.max.k=3, alpha.max.k=3, root.station=TRUE, ip=1, lb=0.000001, ub=1000, max.generations=100, wait.generations=10, pop.size=NULL,print.level=0) {
   #criterion: If aicc or aic, use rgenoud, where the fitness is the score (and try to minimize)
   #    if rjmcmc, use an rjmcmc approach
   #alpha.max.k=3: allows for three OU regimes and one regime where alpha is set to ~0 (brownian motion)
@@ -18,78 +18,78 @@ OUwie.dredge<-function(phy,data, criterion=c("aicc","aic","rjmcmc"), theta.max.k
    dput(starting.values)
    nodes<-Nnode(phy,internal.only=FALSE)
    Domains<-matrix(c(rep(1,nodes),rep(1,nodes),rep(0,nodes),rep(theta.max.k,nodes),rep(sigma.max.k,nodes),rep(alpha.max.k,nodes)),ncol=2,nrow=3*Nnode(phy,internal.only=FALSE),byrow=FALSE)
-	print(dim(Domains))
  #  results<-genoud(fn=dredge.akaike, starting.values=starting.values, max=FALSE,nvars=3*Nnode(phy,internal.only=FALSE), data.type.int=TRUE,boundary.enforcement=2,Domains=matrix(c(rep(1,Nnode(phy,internal.only=FALSE)),rep(1,Nnode(phy,internal.only=FALSE)),rep(0,Nnode(phy,internal.only=FALSE)),rep(theta.max.k,Nnode(phy,internal.only=FALSE)),rep(sigma.max.k,Nnode(phy,internal.only=FALSE)),rep(alpha.max.k,Nnode(phy,internal.only=FALSE))),ncol=2,nrow=3*Nnode(phy,internal.only=FALSE),byrow=FALSE), wait.generations=wait.generations, max.generations=max.generations, pop.size=pop.size, root.station=TRUE, ip=ip, lb=lb, ub=ub, phy, data, criterion=criterion)
-   results<-genoud(fn=dredge.akaike, max=FALSE,nvars=3*nodes, data.type.int=TRUE, boundary.enforcement=2, Domains=Domains, wait.generations=wait.generations, max.generations=max.generations, pop.size=pop.size, root.station=TRUE, ip=ip, lb=lb, ub=ub, phy=phy, data=data, criterion=criterion)
+   results<-genoud(fn=dredge.akaike, max=FALSE,nvars=3*nodes, data.type.int=TRUE, print.level=print.level, boundary.enforcement=2, Domains=Domains, wait.generations=wait.generations, max.generations=max.generations, pop.size=pop.size, root.station=TRUE, ip=ip, lb=lb, ub=ub, phy=phy, data=data, criterion=criterion)
   }
 }
 
+dredge.util<-function(rgenoud.individual) {
+	return(list(k.theta=max(rgenoud.individual[1:(length(rgenoud.individual)/3)]),k.sigma=max(rgenoud.individual[(1+length(rgenoud.individual)/3):(2*length(rgenoud.individual)/3)]),k.alpha=max(rgenoud.individual[(1+2*length(rgenoud.individual)/3):length(rgenoud.individual)])))
+}
+
 dredge.akaike<-function(rgenoud.individual, phy, data, criterion="aicc",lb,ub,ip,root.station,...) {
-  print("in dredge akaike")
-  print(dim(data))
   #first check that the rgenoud.individual is well-structured: don't have just states 0 and 3 for sigma mapping, for example
   #if it fails this, reject it.
   #convert phy+regenoud.individual to simmap.tree (later, make it so that we directly go to the proper object)
   edge.mat.all<-edge.mat(phy,rgenoud.individual)
   #call dev.optimize
-  lnL<-dev.optimize(edges.ouwie=edge.mat.all$edges.ouwie, regimes.mat=edge.mat.all$regime, data=data,root.station=root.station,lb=lb, ub=ub, ip=ip)
+  lnL<-dev.optimize(edges.ouwie=edge.mat.all$edges.ouwie, regimes.mat=edge.mat.all$regime, data=data,root.station=root.station,lb=lb, ub=ub, ip=ip, phy=phy)
+  print(paste('dredge.akaike lnL is ',lnL))
   #which is an nloptr wrapper to call dev.dredge
   #convert likelihood to AICC
   K<-sum(apply(edge.mat.all$regime, 2, max))
-  result<-2*lnL + 2*K
+  print(paste("K is ",K))
+  result<-(2*lnL) + (2*K)
   if (criterion=="aicc") {
     n=Ntip(phy)
-   result<-2*lnL + 2*K * n / (n - K - 1)
+    print(paste("n is ",n))
+   result<-2*lnL + (2*K * n / (n - K - 1))
   }
+  print(paste("AIC or AICC is ",result))
   #return AICC
   return(result)
 }
 
 
-dev.optimize<-function(edges.ouwie,regimes.mat,data,root.station,lb,ub,ip) {
-print("in dev.optimize")
-print(dim(data))
+dev.optimize<-function(edges.ouwie,regimes.mat,data,root.station,lb,ub,ip,phy=phy) {
   np<-sum(apply(regimes.mat, 2, max))
   lower = rep(lb, np)
   upper = rep(ub, np)
   ip<-ip
-  opts <- list("algorithm"="NLOPT_LN_SBPLX", "maxeval"="1000000", "ftol_rel"=.Machine$double.eps^0.5, "xtol_rel"=.Machine$double.eps^0.5)
-  out = nloptr(x0=rep(ip, length.out = np), eval_f=dev.dredge, opts=opts, data=data, root.station=root.station, lb=lower, ub=upper, edges.ouwie=edges.ouwie, regimes.mat=regimes.mat)
+#  opts <- list("algorithm"="NLOPT_LN_SBPLX", "maxeval"="1000000", "ftol_rel"=.Machine$double.eps^0.5, "xtol_rel"=.Machine$double.eps^0.5)
+  opts <- list("algorithm"="NLOPT_LN_SBPLX", "maxeval"="10", "ftol_rel"=.Machine$double.eps^0.5, "xtol_rel"=.Machine$double.eps^0.5)
+
+  out = nloptr(x0=rep(ip, length.out = np), eval_f=dev.dredge, opts=opts, data=data, phy=phy,root.station=root.station, lb=lower, ub=upper, edges.ouwie=edges.ouwie, regimes.mat=regimes.mat)
+  print(-1*out$objective)
   return(-1*out$objective) 
 }
 
 
 #edges.ouwie is the edges matrix with the extra columns for regimes
-dev.dredge<-function(p,edges.ouwie,regimes.mat,data,root.station) { 
-print("in dev.dredge")
-print(dim(data))
+dev.dredge<-function(p,edges.ouwie,regimes.mat,data,root.station,phy) { 
   nRegimes<-dim(regimes.mat)[1]
   Rate.mat.full <- matrix(0, 3, nRegimes) #first row is alpha, second row is sigma, third is theta. Value of zero initially
   k.theta<-max(regimes.mat[,1])
   k.sigma<-max(regimes.mat[,2])
   k.alpha<-max(regimes.mat[,3])
-  p<-c(1:length(p)) #CHANGE AFTER DEBUGGING
   p.index<-1
   for(k in sequence(k.theta)) {
-    Rate.mat.full[1,which(regimes.mat[1,]==k)]<-p[p.index]
+    Rate.mat.full[1,which(regimes.mat[,1]==k)]<-p[p.index]
     p.index<-p.index+1
   }
   for(k in sequence(k.sigma)) {
-    Rate.mat.full[2,which(regimes.mat[2,]==k)]<-p[p.index]
+    Rate.mat.full[2,which(regimes.mat[,2]==k)]<-p[p.index]
     p.index<-p.index+1
   }
   for(k in sequence(k.alpha)) {
-    Rate.mat.full[3,which(regimes.mat[3,]==k)]<-p[p.index]
+    Rate.mat.full[3,which(regimes.mat[,3]==k)]<-p[p.index]
     p.index<-p.index+1
   }
-  print(Rate.mat.full)
   Rate.mat<-Rate.mat.full[1:2,]
-  print(dim(regimes.mat))
-  root.state<-which(regimes.mat[dim(regimes.mat)[1] , ]==1)-4
+  root.state<-NULL
   edges.ouwie.no.root<-edges.ouwie[1:(dim(edges.ouwie)[1]-1),]
   x<-as.matrix(data[,1])
   N<-length(x[,1])
-  print(Rate.mat)
   V<-varcov.ou(phy, edges.ouwie.no.root, Rate.mat, root.state=root.state)
   W<-weight.mat(phy, edges.ouwie.no.root, Rate.mat, root.state=root.state, assume.station=root.station)
   theta<-Rate.mat.full[3,]
@@ -99,7 +99,7 @@ print(dim(data))
   res<-W%*%theta-x		
   q<-t(res)%*%solve(V,res)
   logl <- -.5*(N*log(2*pi)+as.numeric(DET$modulus)+q[1,1])
-  
+  print(paste("p = ",paste(p,sep=" ",collapse=" "),"-logl = ",-logl))
   return(-logl)
 }
 
@@ -143,7 +143,7 @@ edge.mat<-function(phy,rgenoud.individual){
   #Convert to numeric:
   tmp<-as.numeric(tmp)
   #Creates the regime.mat for internal use:
-  obj$regime.mat<-matrix(as.numeric(unlist(strsplit(reg.list," "))),length(reg.list),3)
+  obj$regime.mat<-matrix(as.numeric(unlist(strsplit(reg.list," "))),length(reg.list),3,byrow=TRUE)
   
   regime <- matrix(0,nrow=length(tmp),ncol=length(unique(tmp)))
   #Generates an indicator matrix from the regime vector
--- pkg/R/weight.mat.R
+++ pkg/R/weight.mat.R
@@ -6,6 +6,10 @@ weight.mat<-function(phy, edges, Rate.mat, root.state, simmap.tree=FALSE, assume
 	
 	n=max(phy$edge[,1])
 	ntips=length(phy$tip.label)
+	if(is.null(root.state)) {
+	  root.state<-which(edges[dim(edges)[1] , ]==1)-4 
+	  edges<-edges[-1*dim(edges)[1],]
+	}
 	if(simmap.tree==TRUE){
 		k=length(colnames(phy$mapped.edge))
 	}
